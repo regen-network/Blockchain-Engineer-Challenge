5532c8058a48d60d048cdbdac7bc09f548f9f2a4
eleijonmarck <eric.leijonmarck@gmail.com>
2022-06-20 18:11:28 +0200

    Fix: proto-gen command update to buf alpha protoc

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -19,7 +19,7 @@ protoc_gen_gocosmos
 
 proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
-  buf protoc \
+  buf alpha protoc \
   -I "proto" \
   -I "third_party/proto" \
   --gocosmos_out=plugins=interfacetype+grpc,\
@@ -27,7 +27,7 @@ Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
   $(find "${dir}" -maxdepth 1 -name '*.proto')
 
   # command to generate gRPC gateway (*.pb.gw.go in respective modules) files
-  buf protoc \
+  buf alpha protoc \
   -I "proto" \
   -I "third_party/proto" \
   --grpc-gateway_out=logtostderr=true:. \
